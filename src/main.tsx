4fa98d5f3fe0e3af8d6349bd1c97c826adc79bdd
AndreiLubinets <a.lubinets95@gmail.com>
2024-05-22 21:00:46 +0300

    Changed ui colors

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./styles.css";
 
-document.addEventListener('contextmenu', event => event.preventDefault());
+//document.addEventListener('contextmenu', event => event.preventDefault());
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
