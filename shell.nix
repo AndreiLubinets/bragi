dc2aa7c7824af3faecc8ba12785edf95d9b6348e
Andrei Lubinets <93917217+AndreiLubinets@users.noreply.github.com>
2024-12-06 13:10:20 +0300

    Delete shell.nix

--- shell.nix
+++
@@ -1,5 +0,0 @@
-{ pkgs ? import <nixpkgs> {} }:
-  pkgs.mkShell {
-    nativeBuildInputs = with pkgs.buildPackages; [ pkg-config libsoup_3 pango gdk-pixbuf atkmm webkitgtk_4_1 alsa-lib openssl gtk4 ];
-    GSETTINGS_SCHEMA_DIR="${pkgs.gtk3}/share/gsettings-schemas/${pkgs.gtk3.name}/glib-2.0/schemas";
-}
